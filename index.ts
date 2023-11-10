c97ff5c7a524b6d63ffa0cb8d73b27a8af18cdfe
iqraasaleem <146679065+iqraasaleem@users.noreply.github.com>
2023-11-10 22:48:49 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+import inquirer  from "inquirer";
+import  chalk  from "chalk";
+
+
+let genNum = Math.floor(Math.random() * 2);
+
+let user = await inquirer.prompt({
+    name: 'num1',
+    type: "list",
+    message: "Please Select the Number:",
+    choices: ["0","1","3","4"],
+});
+
+if (genNum == parseInt(user.num1)) {
+    console.log(chalk.bold.greenBright("Congratulations You Won"));
+}
+else {
+    console.log(chalk.bold.redBright("Sorry Better Luck Nexttime!"));}
+   
